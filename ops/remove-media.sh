32472c6f2dead3f43b3414b6448c760b3fd6c6f9
bohendo <bohendo@pm.me>
2021-04-01 17:15:57 +0530

    init remove media helper

---
+++ ops/remove-media.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+fail="❌"
+good="✅"
+
+media=${MEDIA_DIR:-$HOME/Media} 
+trash=$media/.trash
+
+if [[ ! -f "$trash" ]]
+then touch "$trash"
+fi
+
+target="$1"
+if ! [[ "$target" =~ ^[0-9]{8}-[0-9]{6}-[0-9a-f]{8}\.[0-9a-z]{2,5}$ ]]
+then echo "$fail Invalid target provided: $target" && exit 1
+fi
+
+path="$media/$target"
+if ! [[ -f "$path" ]]
+then echo "$fail Can't find a target at $path" && exit 1
+fi
+
+full_digest=$(sha256sum "$path" | cut -d " " -f 1)
+
+if ! grep -qs "$full_digest" < "$trash"
+then echo "$full_digest" >> "$trash"
+fi
+
+rm -rf "$path"
+echo "$good Removed $path"
