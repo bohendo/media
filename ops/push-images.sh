315daef9a43db9ec54cd1c1c824d559b6e689015
bohendo <bohendo@pm.me>
2021-07-29 15:14:53 +0530

    add --fix flag to redate media during validation

--- ops/push-images.sh
+++ ops/push-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
+root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)
 project=$(grep -m 1 '"name":' "$root/package.json" | cut -d '"' -f 4)
 registryRoot=$(grep -m 1 '"registry":' "$root/package.json" | cut -d '"' -f 4)
 organization="${CI_PROJECT_NAMESPACE:-$(whoami)}"
--- ops/tag-images.sh
+++ ops/tag-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
+root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)
 project=$(grep -m 1 '"name":' "$root/package.json" | cut -d '"' -f 4)
 commit=$(git rev-parse HEAD | head -c 8)
 semver=v$(grep -m 1 '"version":' package.json | cut -d '"' -f 4)
